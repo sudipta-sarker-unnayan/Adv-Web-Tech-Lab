f48075977437a752498488837c486864a8257312
Sudipta Sarker Unnayan <22-47656-2@student.aiub.edu>
2026-07-17 19:39:32 +0600

    Lab Task 03

---
+++ lab03/src/course/course.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CourseService } from './course.service';
+
+@Controller('course')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+  getAllCourses() {
+    return this.courseService.getAllCourses();
+  }
+
+  @Get(':id')
+  getCourseById(@Param('id') id: string) {
+    return this.courseService.getCourseById(id);
+  }
+
+  @Post()
+  createCourse(
+    @Body('name') name: string,
+    @Body('code') code: string,
+  ) {
+    return this.courseService.createCourse(name, code);
+  }
+}
