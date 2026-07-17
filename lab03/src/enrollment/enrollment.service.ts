d321d8776f2d7d58db56bc0825424cdf87189840
Sudipta Sarker Unnayan <22-47656-2@student.aiub.edu>
2026-07-17 21:19:18 +0600

    Completed Lab Task 03 - Dependency Injection

--- lab03/src/enrollment/enrollment.service.ts
+++ lab03/src/enrollment/enrollment.service.ts
@@ -1,17 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { CourseService } from '../course/course.service';
+import { NotificationService } from '../notification/notification.service';
 
 @Injectable()
 export class EnrollmentService {
-    constructor(private courseService: CourseService) {}
+  constructor(
+    private courseService: CourseService,
+
+    @Inject(forwardRef(() => NotificationService))
+    private notificationService: NotificationService,
+  ) {}
 
   enrollStudent(studentName: string, courseId: string) {
     const course = this.courseService.getCourseById(courseId);
+
+    const notification = this.notificationService.sendNotification(
+      studentName,
+      'Enrollment Successful',
+    );
 
     return {
       message: 'Student enrolled successfully',
       student: studentName,
       course,
+      notification,
     };
   }
 
